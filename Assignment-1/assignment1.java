441742367f926e30cb121aa8f5e91d4e4b9fb8d9
Mrigank Sinha <sinhamrigank77@gmail.com>
2026-07-28 00:33:56 +0530

    Added Assignment 1

---
+++ Assignment-1/assignment1.java
@@ -0,0 +1,8 @@
+class assignment1{
+        public static void main(String[] args){
+                 System.out.println("Ohhh Kevin De Bruyne!!");
+                 System.out.println("Legend of Manchester City. Goat midfielder in my opinion");
+                 
+
+                 
+}}
