40c5c2137144f2ad0807dfa2557d95b08bd96d0b
Mrigank Sinha <sinhamrigank77@gmail.com>
2026-07-28 00:37:39 +0530

    Name change
    Changed name from assignment1.java to assignment1_print.java

--- Assignment-1/assignment1_print.java
+++ Assignment-1/assignment1_print.java
@@ -1,4 +1,4 @@
-class assignment1{
+class assignment1_print{
         public static void main(String[] args){
                  System.out.println("Ohhh Kevin De Bruyne!!");
                  System.out.println("Legend of Manchester City. Goat midfielder in my opinion");
